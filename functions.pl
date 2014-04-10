e4589ba01d8a5b72f35c4f93e14a4d3f369610ea
Aybuke Ozdemir <aybuke.147@gmail.com>
2014-04-11 02:34:37 +0300

    json format için ekleme yapıldı.

--- functions.pl
+++ functions.pl
@@ -20,11 +20,24 @@ foreach(@reject){
 }
 
 foreach(@fourth){
-  ($one, $two, $three, $four, $five) = split(/ /);
+  ($one, $two, $three, $four) = split(/ /);
   push (@two, $two);
 }
+
+foreach(@two){
+  ($from1, $from2) = split(/@/);
+  push (@from2, $from2);
+}
+
 for ($i=0; $i<$size; $i++){
 print MYFILE_1 ("$two[$i]  - $second[$i] \n");
 }
 close (MYFILE_1);
+
+open(REJECT, "<reject.txt");
+while(<REJECT>){
+  push(@uniq, $_);
+}
+close(REJECT);
+
 
